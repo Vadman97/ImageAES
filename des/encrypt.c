60bd3e467e1ff9d8bbab787a75286d39498bfe19
Prachi Nawathe <pnawathe@usc.edu>
2017-04-14 15:15:13 -0700

    remove left/right array refs add shift arrays

--- des/encrypt.c
+++ des/encrypt.c
@@ -5,19 +5,21 @@ using namespace std;
 int keySize = 16;
 char* message = "waterbot";
 char* key = "hellodar";
-char c[16][28];
-char d[16][28];
+char c[17][28];
+char d[17][28];
+char tempShift[56];
 char* encrypted;
 void encrypt();
-int pc1[56] = {57,   49,    41,   33,    25,    17,    9,
-                1,   58,    50,   42,    34,    26,   18,
-               10,    2,    59,   51,    43,    35,   27,
-               19,   11,     3,   60,    52,    44,   36,
-               63,   55,    47,   39,    31,    23,   15,
-                7,   62,    54,   46,    38,    30,   22,
-               14,    6,    61,   53,    45,    37,   29,
-               21,   13,     5,   28,    20,    12,    4};
-
+void loadShiftArray();
+void shift(int);
+int pc1[56] = {57,    49,    41,   33,    25,    17,    9,   1,
+               58,    50,    42,   34,    26,    18,   10,   2,
+               59,    51,    43,   35,    27,    19,   11,   3,
+               60,    52,    44,   36,    63,    55,   47,  39,
+               31,    23,    15,    7,    62,    54,   46,  38,
+               30,    22,    14,    6,    61,    53,   45,  37,
+               29,    21,    13,    5,    28,    20,   12,   4};
+int cdShift[16] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
 
 int main(void) {
 
@@ -26,11 +28,6 @@ int main(void) {
 }
 
 void encrypt() {
-  char l[keySize / 2];
-  char r[keySize / 2];
-  copy(key, key + 4, l);
-  copy(key + 4, key + 8, r);
-  cout << l << " " << r << endl;
 
   unsigned char k[56];
   //first permuted key
@@ -44,11 +41,20 @@ void encrypt() {
   }
   cout << endl;
   //end first permuted key
-
+  //load permuted key into tempShift to turn into c/d arrays
+  loadShiftArray();
   //begin Cn and Dn array construction
-  for(int i = 0; i < 16; i++) {
+  for(int i = 0; i < 17; i++) {
     copy(k, k + 28, c[i]);
     copy(k + 28, k + 56, d[i]);
   }
+
+}
+
+void loadShiftArray() {
+  copy(k, k + 56, tempShift);
+}
+
+void shift(int i) {
 
 }
