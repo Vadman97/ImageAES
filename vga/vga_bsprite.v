74b5e6b4e57cb6ae1459df9159872348eb7eeeb5
Vadim Korolik <vkorolik@gmail.com>
2017-04-18 01:00:47 -0700

    working on verilog decrypter setup

--- vga/vga_bsprite.v
+++ vga/vga_bsprite.v
@@ -18,44 +18,59 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module vga_bsprite(x0, y0, x1, y1, hc, vc, mem_value, image_width, scaler, rom_addr, R, G, B, blank);
-	input [10:0] x0, y0, x1, y1;	// Coordinates of where the image will be placed
+module vga_bsprite(hc, vc, mem_value, rom_addr, R, G, B, blank, inside_image);
 	input [10:0] hc, vc;	// Coordinates of the current pixel
 	input [7:0] mem_value;	// Memory value at address "rom_addr"
-	input [7:0] image_width;
-	input [3:0] scaler;
 	input blank;
 	output reg [14:0] rom_addr;	// ROM address
 	output reg [2:0] R, G;
 	output reg [1:0] B;	// RGB values outputs;
+	output reg inside_image;
+	
+	parameter [10:0] VGA_WIDTH = 640;
+	parameter [10:0] VGA_HEIGHT = 480;
+	
+	parameter [7:0] IMG_WIDTH = 181;
+	parameter [7:0] IMG_HEIGHT = 181;
+	
+	parameter [3:0] SCALE = 1;
+	
+	parameter [10:0] START_X = (VGA_WIDTH - (IMG_WIDTH * SCALE)) / 2;
+	parameter [10:0] START_Y = (VGA_HEIGHT - (IMG_HEIGHT * SCALE)) / 2;
+	parameter [10:0] END_X = START_X + IMG_WIDTH;
+	parameter [10:0] END_Y = START_Y + IMG_HEIGHT;
 	
 	reg [10:0] x, y;
 	
 	wire [10:0] div_hc, div_vc;
 	wire [11:0] mult_x1, mult_y1;
 	
-	assign div_hc = hc >> (scaler >> 1);
-	assign div_vc = vc >> (scaler >> 1);
-	assign mult_x1 = x1 << (scaler >> 1);
-	assign mult_y1 = y1 << (scaler >> 1);
+	assign div_hc = hc >> (SCALE >> 1);
+	assign div_vc = vc >> (SCALE >> 1);
+	assign mult_x1 = END_X << (SCALE >> 1);
+	assign mult_y1 = END_Y << (SCALE >> 1);
 	
 	always @ (*) begin
 	
-		if (hc >= x0 & hc <= mult_x1)		// make sure thath x1-x0 = image_width
-			x = div_hc - x0;	// offset the coordinates
+		if (hc >= START_X & hc <= mult_x1)		// make sure thath x1-x0 = image_width
+			x = div_hc - START_X;	// offset the coordinates
 		else
 			x = 0;
 			
-		if (vc >= y0 & vc <= mult_y1)		// make sure that y1-y0 = image_height
-			y = div_vc - y0;	//offset the coordinates
+		if (vc >= START_Y & vc <= mult_y1)		// make sure that y1-y0 = image_height
+			y = div_vc - START_Y;	//offset the coordinates
 		else
 			y = 0;
 			
-		rom_addr = y*image_width + x;
+		rom_addr = y*IMG_WIDTH + x;
 		
-		if (x==0 | y==0)		// set the color output
+		if (x==0 | y==0) begin		// set the color output
 			{R,G,B} = 8'd0;
-		else
+			inside_image <= 1'b0;
+		end else begin
 			{R,G,B} = mem_value;
+			inside_image <= 1'b1;
+		end
+			
 	end
 endmodule
