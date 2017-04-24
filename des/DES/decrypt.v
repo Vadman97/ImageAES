5e099a840f5084e7a7c6e5a7b2d79320c5dd5739
Prachi Nawathe <pnawathe@usc.edu>
2017-04-23 17:24:38 -0700

    finished fsm

--- des/DES/decrypt.v
+++ des/DES/decrypt.v
@@ -40,6 +40,7 @@ reg [55:0] k; //initial key permutation
 reg [16:0][27:0] c, d; //shift arrays -- made with key
 reg [15:0][47:0] permutedKey; //list of all permuted keys, used to decrypt
 reg [31:0] r, l; //left right message permutations
+reg [31:0] sOfB;
 reg [47:0] l_e_xor_k;
 reg [31:0] f;
 reg [5:0] decrypt;
@@ -115,25 +116,44 @@ int s[8][4][16] = {
 
                   };
 
-/*integer mp[63:0] = {58,    50,   42,    34,    26,   18,    10,    2,
+integer p[31:0] = {        16,   7,  20,  21,
+                         29,  12,  28,  17,
+                          1,  15,  23,  26,
+                          5,  18,  31,  10,
+                          2,   8,  24,  14,
+                         32,  27,   3,   9,
+                         19,  13,  30,   6,
+                         22,  11,   4,  25};
+
+integer mp[63:0] = {58,    50,   42,    34,    26,   18,    10,    2,
 							60,    52,   44,    36,    28,   20,    12,    4,
 							62,    54,   46,    38,    30,   22,    14,    6,
 							64,    56,   48,    40,    32,   24,    16,    8,
 							57,    49,   41,    33,    25,   17,     9,    1,
 							59,    51,   43,    35,    27,   19,    11,    3,
 							61,    53,   45,    37,    29,   21,    13,    5,
-							63,    55,   47,    39,    31,   23,    15,    7};*/
+							63,    55,   47,    39,    31,   23,    15,    7};
+
+integer ip1[63:0] =  {40,     8,   48,    16,    56,   24,    64,   32,
+                 39,     7,   47,    15,    55,   23,    63,   31,
+                 38,     6,   46,    14,    54,   22,    62,   30,
+                 37,     5,   45,    13,    53,   21,    61,   29,
+                 36,     4,   44,    12,    52,   20,    60,   28,
+                 35,     3,   43,    11,    51,   19,    59,   27,
+                 34,     2,   42,    10,    50,   18,    58,   26,
+                 33,     1,   41,     9,    49,   17,    57,   25};
 
 localparam
-INITIAL = 9'b000000001,
-FIRSTPERMUTATION_MESSAGE_KEY = 9'b000000010,
-LOADSHIFTARRAYS = 9'b000001000,
-CREATEALLKEYS = 9'b000010000,
-CREATE_LE_XOR_K = 9'b000100000,
-CREATE_F = 9'b000100000,
-F_PERMUTE = 9'b001000000,
-DECRYPT = 9'b010000000,
-DONE = 9'b100000000;
+INITIAL = 10'd1,
+FIRSTPERMUTATION_MESSAGE_KEY = 10'd2,
+LOADSHIFTARRAYS = 10'd4,
+CREATEALLKEYS = 10'd8,
+CREATE_LE_XOR_K = 10'd16,
+CREATE_F = 10'd32,
+S_OF_B_PERMUTE = 10'd64,
+F_PERMUTE = 10'd128,
+DECRYPT = 10'd256,
+DONE = 10'd512;
 
 assign key = DESkey;
 assign msg = message; 
@@ -161,6 +181,7 @@ always @ (posedge clk, posedge reset)
 				case (state) 
 					INITIAL:
 						begin
+							integer i; 
 							//state
 							state <= FIRSTPERMUTATION_MESSAGE_KEY;
 							//rtl
@@ -168,8 +189,10 @@ always @ (posedge clk, posedge reset)
 							key <= DESkey;
 							result <= 0;
 							decrypt <= 0;
-							r <= msg[0:31];
-							l <= msg[32:63];
+							for(i = 0; i < 64; i = i + 1) begin
+								if(i < 32) r[i] <= msg[mp[i] - 1];
+								else l[i - 32] <= msg[mp[i] - 1];
+							end
 							l_e_xor_k <= 48'b0;
 						end
 					FIRSTPERMUTATION_MESSAGE_KEY:
@@ -228,9 +251,8 @@ always @ (posedge clk, posedge reset)
 							integer i; 
 							reg [47:0] L_e;
 							//state
-							state <= CREATE_F;
+							state <= S_OF_B_PERMUTE;
 							//rtl
-							r <= l;
 							for(i = 0; i < 48; i = i + 1) begin
 								L_e = l[ebit[i] - 1] ^ permutedKey[15 - decrypt][i];
 								
@@ -240,30 +262,48 @@ always @ (posedge clk, posedge reset)
 							l_e_xor_k <= L_e;
 							// l(n) = r (n - 1) + f(l (n), k (n))
 						end
-					CREATE_F:
+					S_OF_B_PERMUTE: 
 						begin
-							reg[31:0] pOfS;
 							integer i;
+							//state
+							state <= F_PERMUTE;
+							//rtl 
+							for(i = 0; i < 8; i = i + 1) begin
+								reg [1:0] index;
+								reg [3:0] val; 
+								sOfB[i * 4: i * 4 + 3] <= 
+									s[i][l_e_xor_k[i * 6] << 1 + l_e_xor_k[i * 6 + 5]][l_e_xor_k[i * 6 + 1:i * 6 + 5]];
+							end
+						end
+					F_PERMUTE:
+						begin
+							integer i; 
 							//state
 							if(decrypt == 15)
 								state <= DECRYPT;
 							else state <= CREATE_LE_XOR_K;
-							//rtl 
+							//rtl
 							decrypt <= decrypt + 1; 
-							for(i = 0; i < 8; i = i + 1) begin
-								reg [1:0] index;
-								reg [3:0] val; 
-								index = l_e_xor_k[i * 6] << 1 + l_e_xor_k[i * 6 + 5];
-								val = {l_e_xor_k[1:4]};
-								pOfS[i * 4: i * 4 + 3] = s[i][index][val];
+							for(i = 0; i < 32; i = i + 1) begin
+								f[i] <= sOfB[p[i] - 1];
+								l[i] <= r[i] ^ sOfB[p[i] - 1];
 							end
-							
+							r <= l; 
 						end
 					DECRYPT: 
 						begin
+							integer i; 
+							//state 
+							state <= DONE;
+							//rtl
+							for(i = 0; i < 64; i = i + 1) begin
+								if(ip1[i] - 1 < 32) result[i] <= l[ip1[i] - 1];
+								else result[i] <= r[ip1[i] - 33];
+							end
 						end
 					DONE: 
 						begin
+							if(ack) state <= INITIAL;
 						end
 				endcase
 			end
